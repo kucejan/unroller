c495956b63a0778e88206efa8fc91298ff6d3e4a
Jan Kučera <jan.kucera@cesnet.cz>
2020-05-13 13:57:45 +0200

    Remove topologies from the repo, add README with steps to download them

--- topologies/topology-zoo/cmds.sh
+++
@@ -1,7 +0,0 @@
-# install tools
-pip install topzootools
-
-# convert rocketfuel dataset into zoo format
-wget http://www.cs.washington.edu/research/networking/rocketfuel/maps/policy-dist.tar.gz
-tar -xzf policy-dist.tar.gz
-rocketfuel2zoo -d maps-n-paths/
